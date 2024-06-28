4c677f3068494042be1ac57c8be5b1f7c07584dc
Gabriel Carrillo <agabrielcarrillo01@gmail.com>
2024-06-28 17:56:41 -0300

    se añade level 6

--- Scenes/UI/Menus/level_select.gd
+++ Scenes/UI/Menus/level_select.gd
@@ -33,3 +33,28 @@ func _process(delta):
 func _on_quit_button_pressed():
 	get_tree().change_scene_to_file("res://Scenes/UI/Menus/main_menu.tscn")
 	pass # Replace with function body.
+	
+	
+
+func _on_lv_1_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Levels/test_level.tscn")
+	pass # Replace with function body.
+	
+	
+
+func _on_lv_2_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Levels/test_level_2.tscn")
+	pass # Replace with function body.
+	
+	
+
+func _on_lv_3_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Levels/level_3.tscn")
+	pass # Replace with function body.
+	
+	
+
+
+func _on_lv_4_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Levels/level_4.tscn")
+	pass # Replace with function body.
