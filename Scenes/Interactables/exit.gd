5893f31e0f38dfaeec668692ff4f9ca4148de21b
Lucas Bloise <lucas.bloise@qubika.com>
2024-06-30 20:32:09 -0300

    add correct exit + credits

--- Scenes/Interactables/exit.gd
+++ Scenes/Interactables/exit.gd
@@ -11,9 +11,11 @@ func _on_body_entered(body):
 		
 func changeLevel():
 	var current_scene_file = get_tree().current_scene.scene_file_path
-	print(current_scene_file)
 	var next_level_number = current_scene_file.to_int() + 1
-	print(next_level_number)
-	var next_level_path = FILE_BEGIN + str(next_level_number) + ".tscn"
-	print(next_level_path)
-	get_tree().change_scene_to_file(next_level_path)
+
+	if next_level_number == 7:
+		get_tree().change_scene_to_file("res://Scenes/UI/Menus/credits.tscn")
+	else:
+		var next_level_path = FILE_BEGIN + str(next_level_number) + ".tscn"
+		get_tree().change_scene_to_file(next_level_path)
+	
